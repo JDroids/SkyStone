717f7552c3a251ce5d6b97bc8034d01f6d52533a
dansman805 <dansman805@gmail.com>
2019-12-14 21:14:14 -0500

    Made TeleOp deploy intake on init

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.kt
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import com.qualcomm.robotcore.hardware.Servo
 import org.firstinspires.ftc.teamcode.bot.commands.teleop.ArcadeDrive
 import org.firstinspires.ftc.teamcode.bot.commands.teleop.ControlDeposit
 import org.firstinspires.ftc.teamcode.bot.commands.teleop.ControlIntake
@@ -8,9 +9,18 @@ import org.firstinspires.ftc.teamcode.bot.commands.teleop.ControlFoundation
 
 @TeleOp
 class TeleOp : OpModeTemplate(shouldIntakeDeploy = true) {
-    override fun initOpMode() {}
+    private val intakePivotLeft by lazy {hardwareMap.get(Servo::class.java, "intakeDownL")}
+    private val intakePivotRight by lazy {hardwareMap.get(Servo::class.java, "intakeDownR")}
+
+    override fun initOpMode() {
+        intakePivotLeft
+        intakePivotRight
+    }
 
     override fun run() {
+        intakePivotLeft.position = 1.0
+        intakePivotRight.position = 1.0
+
         commander.runCommandsParallel(
                 ArcadeDrive(drive, gamepad1),
                 ControlIntake(intake, gamepad2),
