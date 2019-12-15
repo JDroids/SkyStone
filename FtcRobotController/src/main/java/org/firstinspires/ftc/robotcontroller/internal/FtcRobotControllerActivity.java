8467c4648dbc28a0ab81c5dc6aa1b43e40462b85
dansman805 <dansman805@gmail.com>
2019-12-14 21:35:43 -0500

    Fixed FtcRobotControllerActivity

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/FtcRobotControllerActivity.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/FtcRobotControllerActivity.java
@@ -61,9 +61,6 @@ import android.widget.PopupMenu;
 import android.widget.TextView;
 
 import com.acmerobotics.dashboard.FtcDashboard;
-import com.google.blocks.ftcrobotcontroller.BlocksActivity;
-import com.google.blocks.ftcrobotcontroller.ProgrammingModeActivity;
-import com.google.blocks.ftcrobotcontroller.ProgrammingModeControllerImpl;
 import com.google.blocks.ftcrobotcontroller.ProgrammingWebHandlers;
 import com.google.blocks.ftcrobotcontroller.runtime.BlocksOpMode;
 import com.qualcomm.ftccommon.ClassManagerFactory;
