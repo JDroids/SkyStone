bb02ac0aaa9a28d5f64e8e960dd1ce5b41453fa3
WhyIsItBroken <hawkstarthunder@gmail.com>
2019-12-27 19:59:18 -0500

    Turn90Degrees.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Turn90Degrees.java
@@ -0,0 +1,91 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@Autonomous(name = "Turn90Degrees", group = "")
+public class Turn90Degrees extends LinearOpMode {
+
+    private BNO055IMU imu;
+
+    /**
+     * This function is executed when this Op Mode is selected from the Driver Station.
+     */
+
+    private DcMotor frontRight = null;
+    private DcMotor frontLeft = null;
+    private DcMotor backRight = null;
+    private DcMotor backLeft = null;
+
+    @Override
+    public void runOpMode() {
+
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+
+        BNO055IMU.Parameters imuParameters;
+        Orientation angles;
+        Acceleration gravity;
+
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+
+        frontLeft.setDirection(DcMotor.Direction.REVERSE);
+        frontRight.setDirection(DcMotor.Direction.FORWARD);
+        backLeft.setDirection(DcMotor.Direction.REVERSE);
+        backRight.setDirection(DcMotor.Direction.FORWARD);
+
+        // Create new IMU Parameters object.
+        imuParameters = new BNO055IMU.Parameters();
+        // Use degrees as angle unit.
+        imuParameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        // Express acceleration as m/s^2.
+        imuParameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        // Disable logging.
+        imuParameters.loggingEnabled = false;
+        // Initialize IMU.
+        imu.initialize(imuParameters);
+        // Prompt user to press start buton.
+        waitForStart();
+        if (opModeIsActive()) {
+
+
+            while (opModeIsActive()) {
+
+                // Get absolute orientation
+                // Get acceleration due to force of gravity.
+                angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+                gravity = imu.getGravity();
+
+                if (angles.firstAngle < 90 ){
+
+                    frontLeft.setPower(0.2);
+                    frontRight.setPower(-0.2);
+                    backLeft.setPower(0.2);
+                    backRight.setPower(-0.2);
+
+                    // Display orientation info.
+                    telemetry.addData("Z", angles.firstAngle);
+                    telemetry.update();
+                }
+                else{
+                    frontLeft.setPower(0);
+                    frontRight.setPower(0);
+                    backLeft.setPower(0);
+                    backRight.setPower(0);
+                }
+            }
+        }
+    }
+}
